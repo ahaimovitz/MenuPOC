edcc00dc6a0db72c33f1a27d2807d4c31e7e9db8
Aaron Haimovitz <ahaimovitz@ahaimovitz-mbook.local>
2021-09-08 12:22:55 -0400

    added firestore

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,34 @@ const express = require("express");
 
 admin.initializeApp();
 
+var ref = admin.database().ref("categories");
+
+ref.once('value', gotUserData);
+
+function gotUserData(snapshot){
+  snapshot.forEach(userSnapshot => {
+    var k = userSnapshot.key;
+    var id = userSnapshot.val().id;
+    var products = userSnapshot.val().products;
+    var prod_keys = Object.keys(products);
+    for(i=0; i<prod_keys.length; i++)
+    {
+        console.log("prodkey=" + products[i].key);
+    }
+    console.log(k + " " + id + " products=" + products);
+   /**  ref.child("teams").child(id).once("value", teamsSnapshot => {
+      teamsSnapshot.forEach(teamSnapshot => {
+        var teamKey = teamSnapshot.key;
+        teamSnapshot.forEach(teamProp => {
+          var prop = teamProp.key;
+          var val = teamProp.val();
+          console.log(k+" "+name+" "+id+": "+teamKey+", "+prop+"="+val);
+        });
+      });
+    });
+    */
+  })
+}
 
 const { ApolloServer, gql } = require("apollo-server-express");
 
@@ -20,7 +48,7 @@ const typeDefs = gql`
   
   type Category {
     name: String
-    id: Int
+    id: Int!
     products: [Product]
    }
 
@@ -38,6 +66,7 @@ const typeDefs = gql`
   
   type Query {
     categories: [Category]
+    getCategory(id: ID!): [Category]
     #  product(availability: Availability): Product 
     availability(now:String): Boolean
   }
@@ -51,12 +80,18 @@ const resolvers = {
           .once("value")
           .then(snap => snap.val())
           .then(val => Object.keys(val).map(key => val[key]))
+          ,
+      getCategory:  (_,_id,__) =>
+        admin.database().ref("categories").orderByChild("name").once("value")
+        .then(snap => snap.val())
+        .then(val => Object.keys(val).map(key => val[key]))
+    
     }
   }; 
 
   console.log(resolvers);
-  /* const scoresRef = admin.database().ref('categories').once("value");
-
+//  const scoresRef = admin.database().ref('categories').once("value");
+/** 
 scoresRef.on('value', (snapshot) => {
   snapshot.forEach((data) => {
     console.log('The ' + data.key + ' dinosaur\'s score is ' + data.val());
